5b7b67a0a0ca61ccfecc6b33184aba5c8cb6c7b2
Arek M <arekmat@poczta.fm>
2021-10-22 21:27:59 +0200

    Fix room 'schema export directory is not provided...' warning

--- tmdb-movie-repository/src/main/java/amaterek/movie/data/local/MoviesDatabase.kt
+++ tmdb-movie-repository/src/main/java/amaterek/movie/data/local/MoviesDatabase.kt
@@ -3,7 +3,7 @@ package amaterek.movie.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [FavoriteMovieEntity::class], version = 1)
+@Database(entities = [FavoriteMovieEntity::class], version = 1, exportSchema = false)
 internal abstract class MoviesDatabase : RoomDatabase() {
 
     internal abstract fun getFavoriteMoviesDao(): FavoriteMoviesDao
